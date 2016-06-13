de7ff2564caa3e9aee4998bb4fdfed1219033a5c
Nordes Ménard-Lamarre <nordes@gmail.com>
2016-06-13 22:37:07 +0200

    Creation of POC
    POC to get azure storage blob (IIS). The idea is only to fetch the blob
    from a certain time and then go on to fetch all until now and continue
    using threads to update the log files.

---
+++ AzureIISBlobToLocalLogs/ConfigSection/AzureBlobFileWatchSection.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Configuration;
+
+namespace AzureIISBlobToLocalLogs.ConfigSection
+{
+    /// <summary>
+    /// https://msdn.microsoft.com/en-us/library/2tw134k3.aspx
+    /// </summary>
+    /// <seealso cref="System.Configuration.ConfigurationSection" />
+    public class AzureBlobFileWatchSection : ConfigurationSection
+    {
+        // Create a "remoteOnly" attribute.
+        [ConfigurationProperty("localLogPath", DefaultValue = "c:\\logs\\", IsRequired = false)]
+        public string LocalLogPath
+        {
+            get
+            {
+                return (string)this["localLogPath"];
+            }
+            set
+            {
+                this["localLogPath"] = value;
+            }
+        }
+
+        [ConfigurationProperty("initFrom", IsRequired = false)]
+        public string InitFrom
+        {
+            get
+            {
+                return (string)this["initFrom"];
+            }
+            set
+            {
+                this["initFrom"] = value;
+            }
+        }
+
+        [ConfigurationProperty("paths", IsDefaultCollection = true)]
+        [ConfigurationCollection(typeof(PathElements), AddItemName = "add", ClearItemsName = "clear", RemoveItemName = "remove")]
+        public PathElements Paths
+        {
+            get
+            {
+                PathElements paths = (PathElements)base["paths"];
+
+                return paths;
+            }
+
+            set
+            {
+                PathElements paths = value;
+            }
+
+        }
+    }
+
+    public class PathElements : ConfigurationElementCollection
+    {
+        public override ConfigurationElementCollectionType CollectionType
+        {
+            get
+            {
+                return ConfigurationElementCollectionType.AddRemoveClearMap;
+            }
+        }
+
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new PathElement();
+        }
+
+        protected override Object GetElementKey(ConfigurationElement element)
+        {
+            return ((PathElement)element).Name;
+        }
+
+        public PathElement this[int index]
+        {
+            get
+            {
+                return (PathElement)BaseGet(index);
+            }
+            set
+            {
+                if (BaseGet(index) != null)
+                {
+                    BaseRemoveAt(index);
+                }
+                BaseAdd(index, value);
+            }
+        }
+
+        new public PathElement this[string Name]
+        {
+            get
+            {
+                return (PathElement)BaseGet(Name);
+            }
+        }
+
+        protected override void BaseAdd(ConfigurationElement element)
+        {
+            BaseAdd(element, false);
+
+            // Your custom code goes here.
+
+        }
+
+        public void RemoveAt(int index)
+        {
+            BaseRemoveAt(index);
+
+            // Your custom code goes here.
+
+        }
+
+        public void Remove(string name)
+        {
+            BaseRemove(name);
+
+            // Your custom code goes here.
+
+        }
+
+        public void Clear()
+        {
+            BaseClear();
+
+            // Your custom code goes here.
+            Console.WriteLine("UrlsCollection: {0}", "Removed entire collection!");
+        }
+
+    }
+
+    public class PathElement : ConfigurationElement
+    {
+        //[StringValidator(InvalidCharacters = "~!@#$%^&*()[]{}/;'\"|\\", MinLength = 1, MaxLength = 60)]
+        [ConfigurationProperty("name", IsRequired = true)]
+        public String Name
+        {
+            get
+            {
+                return (String)this["name"];
+            }
+            set
+            {
+                this["name"] = value;
+            }
+        }
+
+        [ConfigurationProperty("value", IsRequired = true)]
+        public string Value
+        {
+            get
+            { return (string)this["value"]; }
+            set
+            { this["value"] = value; }
+        }
+
+        [ConfigurationProperty("container", IsRequired = true)]
+        public string Container
+        {
+            get
+            { return (string)this["container"]; }
+            set
+            { this["container"] = value; }
+        }
+    }
+}
---
+++ AzureIISBlobToLocalLogs/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using AzureIISBlobToLocalLogs.ConfigSection;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Globalization;
+
+namespace AzureIISBlobToLocalLogs
+{
+    /// <summary>
+    /// POC in order to fetch the IIS logs from Azure Storage Blobs (Basically the WebApp or API... stats)
+    /// 
+    /// Idea come originally from http://madstt.dk/iis-logs-in-elasticsearch/, but quite modified
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Global for now, but it should be put elsewhere, like the majority of the code. It's a POC, don't look ^^.
+        /// </summary>
+        private static AzureBlobFileWatchSection _config;
+
+        /// <summary>
+        /// Not yet in use
+        /// </summary>
+        private static readonly IDictionary<string, long> _positionCache = new Dictionary<string, long>();
+
+        static void Main(string[] args)
+        {
+            WarmUp();
+            RunAndFetchData();
+            Console.WriteLine("Push a key");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Should fetch the data periodically having some sleep time
+        /// </summary>
+        private static void RunAndFetchData()
+        {
+            // TODO Idea is to have a configuration with a sleep time. (Min 10sec, max...)
+
+            // Do polling... thread fun starts here.
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Warm up and fetch the logs until now.
+        /// </summary>
+        private static void WarmUp()
+        {
+            GetAzureSection();
+            FetchBlobsUntilNow();
+        }
+
+        private static void FetchBlobsUntilNow()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("storageIIS"));
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            DateTime iisLogFetchDateTime = GetStartDateFromConfig();
+
+            // Retrieve a reference to a container.
+            foreach (PathElement pathElement in _config.Paths)
+            {
+                CloudBlobContainer container = blobClient.GetContainerReference(pathElement.Container);
+                var blobs = new List<CloudBlockBlob>();
+
+                // We want to fetch all until now, after we will attach and fetch only what's needed.
+                while (iisLogFetchDateTime.CompareTo(DateTime.UtcNow) <= 0)
+                {
+                    string hourPath = GetHourPath(pathElement.Value, iisLogFetchDateTime);
+
+                    // Loop over items within the container and output the length and URI.
+                    foreach (IListBlobItem item in container.ListBlobs(hourPath, true))
+                    {
+                        if (item.GetType() == typeof(CloudBlockBlob))
+                        {
+                            CloudBlockBlob cloudBlockBlob = (CloudBlockBlob)item;
+                            Console.WriteLine("Block blob of length {0}: {1} // {2}", cloudBlockBlob.Properties.Length, cloudBlockBlob.Uri, cloudBlockBlob.Properties.ETag);
+                            Console.WriteLine("Blob found: {0}", cloudBlockBlob.Uri);
+                            blobs.Add(cloudBlockBlob);
+
+                            try
+                            {
+                                string fileName = _config.LocalLogPath + container.Name + "-" + iisLogFetchDateTime.ToString("yyyy-MM-dd-HH") + ".log";
+
+                                // TODO have a cleaner code
+                                if (!File.Exists(fileName))
+                                {
+                                    using (var f = File.Create(fileName))
+                                    {
+                                        // For some reason the OpenCreate does not happen since I use file stream. Best to create and then Append.
+                                    }
+                                }
+                                using (var azureBlobStream = cloudBlockBlob.OpenRead())
+                                using (var azureBlobReader = new StreamReader(azureBlobStream))
+                                using (var logFs = new FileStream(fileName, FileMode.Append, FileAccess.Write))
+                                using (var logFw = new StreamWriter(logFs))
+                                {
+                                    WriteIISLogs(cloudBlockBlob, azureBlobReader, logFw);
+                                }
+                            }
+                            catch (IOException)
+                            {
+                                Console.WriteLine("ERROR THROWN");
+                            }
+                        }
+                    }
+
+                    iisLogFetchDateTime = iisLogFetchDateTime.AddHours(1); // IIS Log paths are made by hours
+                }
+            }
+        }
+
+        private static void WriteIISLogs(CloudBlockBlob cloudBlockBlob, StreamReader azureBlobReader, StreamWriter logFw)
+        {
+            // The position will become handy when we want to fetch only the last part of the log.
+            var originalPosition = GetPosition(cloudBlockBlob.Uri.AbsoluteUri);
+            azureBlobReader.BaseStream.Seek(originalPosition, SeekOrigin.Begin);
+
+            while (!azureBlobReader.EndOfStream)
+            {
+                var line = azureBlobReader.ReadLine();
+                if (!line.StartsWith("#"))
+                {
+                    // Append to the log file.
+                    logFw.WriteLine(line);
+                }
+            }
+
+            logFw.Flush();
+            // SavePositionCurrentPosition...
+        }
+
+        private static DateTime GetStartDateFromConfig()
+        {
+            // We should start from choosen date... 
+            DateTime dtStart = DateTime.Today;
+            if (!string.IsNullOrEmpty(_config.InitFrom))
+            {
+                dtStart = DateTime.ParseExact(_config.InitFrom, "yyyy/MM/dd/HH", CultureInfo.InvariantCulture);
+                Console.WriteLine("Start fetching from : {0}", dtStart);
+            }
+
+            return dtStart;
+        }
+
+        private static string GetHourPath(string basePath, DateTime dtStart)
+        {
+            return basePath + "/" + dtStart.ToString("yyyy/MM/dd/HH");
+        }
+
+        private static long GetPosition(string blobUri)
+        {
+            var key = "position_" + blobUri;
+
+            long position;
+            if (!_positionCache.TryGetValue(key, out position))
+            {
+                return 0; // Not found, so let's take the beginning of the file.
+            }
+            return position;
+        }
+
+        /// <summary>
+        /// Get the config section.
+        /// </summary>
+        private static void GetAzureSection()
+        {
+            _config = (ConfigSection.AzureBlobFileWatchSection)System.Configuration.ConfigurationManager.GetSection("azureIISBlobFileWatchGroup/watch");
+        }
+    }
+}
